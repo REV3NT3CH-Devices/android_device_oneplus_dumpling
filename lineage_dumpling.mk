ebd9200012dd0a4fc4284e1c02bb783df58b0eaf
REV3NT3CH <rev3nt3ch@gmail.com>
2023-05-22 21:27:14 -0700

    dumpling: Enable QuickTap & FaceUnlock Rom Feature

--- lineage_dumpling.mk
+++ lineage_dumpling.mk
@@ -11,6 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from dumpling device
 $(call inherit-product, device/oneplus/dumpling/device.mk)
 
+# Enable Rom Features
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
+
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
